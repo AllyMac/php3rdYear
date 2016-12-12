49dd8f726faa2d217254b2dd5172635b34ed6840
AllyMac <alistair.macrae@sky.com>
2016-12-12 11:52:24 +0000

    map test

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html>
+<html lang="en">
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <title>Directions service</title>
+    <title>Go Portlethen</title>
+
     <style>
+
+    <!-- Main CSS file -->
+    <link rel="stylesheet" href="cluster/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="cluster/css/owl.carousel.css" />
+    <link rel="stylesheet" href="cluster/css/magnific-popup.css" />
+    <link rel="stylesheet" href="cluster/css/font-awesome.css" />
+    <link rel="stylesheet" href="cluster/css/style.css" />
+    <link rel="stylesheet" href="cluster/css/responsive.css" />
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="cluster/images/icon/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="cluster/images/icon/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="cluster/images/icon/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="cluster/images/icon/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="cluster/images/icon/apple-touch-icon-57-precomposed.png">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
       #map {
@@ -30,8 +55,254 @@
         padding-left: 10px;
       }
     </style>
+
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+    <script>
+        window.addEventListener("load", function(){
+            window.cookieconsent.initialise({
+                "palette": {
+                    "popup": {
+                        "background": "#3c404d",
+                        "text": "#d6d6d6"
+                    },
+                    "button": {
+                        "background": "#8bed4f"
+                    }
+                },
+                "theme": "edgeless",
+                "position": "bottom-right",
+                "content": {
+                    "message": "Our website uses cookies to make your browsing experience better. By using our site you agree to our use of cookies.",
+                    "dismiss": "Close and don't show again"
+                }
+            })});
+    </script>
+
+
+    <!--Linking the website to to the jQuery library-->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
   </head>
   <body>
+
+
+    <!-- PRELOADER -->
+    <!--<div id="st-preloader">
+        <div id="pre-status">
+            <div class="preload-placeholder"></div>
+        </div>
+    </div>-->
+    <!-- /PRELOADER -->
+
+    <!-- HEADER -->
+    <header id="header">
+
+
+        <nav class="navbar st-navbar navbar-fixed-top">
+
+
+            <!--<div style="text-align:left; font-size:30px;font-family: 'Raleway'" ;>
+                <a class="logo" href="index.php"><b>GO PORTLETHEN</b></a>
+            </div>-->
+
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#st-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+            </button>
+            <nav class="navbar st-navbar navbar-fixed-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#st-navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="logo" href="index.php"><b style="font-size: 20px;">GOPORTLETHEN</b></a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="st-navbar-collapse">
+                        <ul class="nav navbar-nav navbar-left">
+                            <li><a href="#clubs">Clubs</a></li>
+                            <li><a href="discover.php">Discover</a></li>
+                            <li><a href="healthNews.php">healthy living</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+
+
+                            <?php
+                            session_start();
+                            if (isset($_POST["logout"])) {
+                                session_destroy();
+                            }
+
+                            ini_set('display_errors', 1);
+                            ini_set('display_startup_errors', 1);
+                            error_reporting(E_ALL);
+
+
+                            if (isset($_SESSION['loginStatus'])) {
+                                if ($_SESSION['loginStatus'] === TRUE) {
+                                    //check the email and password
+                                    // connect to server and select database
+                                    $db = new mysqli(
+                                        "eu-cdbr-azure-west-a.cloudapp.net",
+                                        "bd2505ec24d031",
+                                        "a0a7a671",
+                                        "goportlethendb"
+                                    );
+                                    // test if connection was established, and print any errors
+                                    if ($db->connect_errno) {
+                                        die('Connectfailed[' . $db->connect_error . ']');
+                                    }
+                                    // create a SQL query as a string
+                                    $sql_query = "SELECT * FROM users WHERE emailAddress='" . $_SESSION['emailAddress'] . "' AND password='" . $_SESSION['password'] . "' LIMIT 1";
+                                    // execute the SQL query
+                                    $result = $db->query($sql_query);
+
+                                    $rowsFound = $result->num_rows;
+
+                                    while ($row = $result->fetch_array()) {
+                                        // print out fields from row of data
+                                        $_SESSION['displayName'] = $row['displayName'];
+                                    }
+
+                                    if ($rowsFound === 1) {
+
+                                        $_SESSION['loginStatus'] = TRUE;
+                                    }
+
+                                    if ($rowsFound === 0) {
+                                        $_SESSION['loginStatus'] = FALSE;
+                                    }
+
+                                    $result->close();
+                                    // close connection to database
+                                    $db->close();
+                                }
+                            }
+
+
+                            if (isset($_POST["signin"])) {
+                                $_SESSION['emailAddress'] = $_POST["email"];
+                                $_SESSION['password'] = $_POST["password"];
+
+                                //check the email and password
+                                // connect to server and select database
+                                $db = new mysqli(
+                                    "eu-cdbr-azure-west-a.cloudapp.net",
+                                    "bd2505ec24d031",
+                                    "a0a7a671",
+                                    "goportlethendb"
+                                );
+                                // test if connection was established, and print any errors
+                                if ($db->connect_errno) {
+                                    die('Connectfailed[' . $db->connect_error . ']');
+                                }
+                                // create a SQL query as a string
+                                $sql_query = "SELECT * FROM users WHERE emailAddress='" . $_SESSION['emailAddress'] . "' AND password='" . $_SESSION['password'] . "' LIMIT 1";
+                                // execute the SQL query
+                                $result = $db->query($sql_query);
+
+                                $rowsFound = $result->num_rows;
+
+                                while ($row = $result->fetch_array()) {
+                                    // print out fields from row of data
+                                    $_SESSION['displayName'] = $row['displayName'];
+                                }
+
+                                if ($rowsFound === 1) {
+
+                                    $_SESSION['loginStatus'] = TRUE;
+                                }
+
+                                if ($rowsFound === 0) {
+                                    $_SESSION['loginStatus'] = FALSE;
+                                }
+
+                                $result->close();
+                                // cl ose connection to database
+                                $db->close();
+                            }
+
+                            if (isset($_SESSION['loginStatus'])) {
+                                //if user is logged in display username
+                                if ($_SESSION['loginStatus'] === TRUE) {
+                                    echo("Logged in as " . $_SESSION['displayName']);
+                                    //if user login was false
+                                } elseif ($_SESSION['loginStatus'] === FALSE && isset($_POST["signin"])) {
+                                    echo('<li style="margin: 14px;"><b>' . "INCORRECT USERNAME OR PASSWORD" . '</b></li>');
+                                } elseif ($_SESSION['loginStatus'] === FALSE) {
+                                    echo('<li style="margin: 14px;"><b>' . "YOU ARE NOT LOGGED IN" . '</b></li>');
+                                }
+                            } else {
+                                echo('<li style="margin: 14px;"><b>' . "YOU ARE NOT LOGGED IN" . '</b></li>');
+                            }
+
+
+                            //if the signup form in submitted from createUser.php
+                            if (isset($_POST["signup"])) {
+                                $name = $_POST["name"];
+                                $password = $_POST["password"];
+                                $emailAddress = $_POST["emailAddress"];
+                                $displayName = $_POST["displayName"];
+
+
+                                // connect to server and select database
+                                $db = new mysqli(
+                                    "eu-cdbr-azure-west-a.cloudapp.net",
+                                    "bd2505ec24d031",
+                                    "a0a7a671",
+                                    "goportlethendb"
+                                );
+                                // test if connection was established, and print any errors
+                                if ($db->connect_errno) {
+                                    die('Connectfailed[' . $db->connect_error . ']');
+                                }
+                                // create a SQL query as a string
+                                $sql_query = "INSERT INTO users (name, password, emailAddress,displayName)
+                                              VALUES ('$name', '$password', '$emailAddress','$displayName')";
+                                // execute the SQL query
+                                if ($db->query($sql_query) === TRUE) {
+                                    echo "Signup successful!";
+                                } else {
+                                    echo "Error: " . $sql_query . "<br>" . $db->error;
+                                }
+
+                                $db->close();
+                            }
+                            ?>
+                            <li>
+
+                                <?php
+                                if (isset($_SESSION['loginStatus']) && $_SESSION['loginStatus'] === TRUE) {
+                                    ?>
+                                    <form action="index.php" method="post" style="margin-top:10px;">
+                                        <input style="font-weight: 600;border-radius: 5px;background-color: lightgray;"
+                                               type="submit" value="LOGOUT" name="logout" class="button">
+                                    </form>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <form action="createUser.php" style="margin-top:10px;" >
+                                        <input style="font-weight: 600;border-radius: 5px;background-color: lightgray;"
+                                               type="submit" value="SIGN IN/SIGN UP" class="button">
+                                    </form>
+                                    <?php
+                                }
+
+                                ?>
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container -->
+            </nav>
+    </header>
+    <!-- /HEADER -->
+
+
     <div id="floating-panel">
     <b>Start: </b>
     <select id="start">
